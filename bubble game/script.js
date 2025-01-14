0be562e900e547a8aa0c5938635072cbe3bb5886
WelRaj <rajkumar.rathor.250925@gmail.com>
2025-01-14 20:11:54 +0530

    commit changes

--- bubble game/script.js
+++ bubble game/script.js
@@ -1,27 +1,49 @@
-function makebubble(){
-var clutter= " ";
-for(var i=1; i<=360; i++){
-       var ab= Math.floor(Math.random()*10);
-       clutter+= `<div class="bubble">${ab}</div>`;   
+
+var score = 0;
+var timer = 60;
+var ab = 0;
+
+function increasescore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
 }
-document.querySelector("#pbtm").innerHTML= clutter;
+
+function makebubble() {
+  var clutter = "";
+  for (var i = 1; i <= 360; i++) {
+    var randomNumber = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${randomNumber}</div>`;
+  }
+  document.querySelector("#pbtm").innerHTML = clutter;
 }
-var timer=60;
-function gethit(){
-    var ab = Math.floor(Math.random()*10);
-    document.querySelector("#hittime").textContent=ab;
+
+function gethit() {
+  ab = Math.floor(Math.random() * 10);
+  document.querySelector("#hittime").textContent = ab;
 }
-function runtimer(){
-        var timeint= setInterval(function(){
-              if(timer>0){
-                timer--;
-                document.querySelector("#timerval").textContent=timer;
-              }
-              else{
-                clearInterval(timeint);
-              }
-         },1000);
+
+function runtimer() {
+  var timeint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearInterval(timeint);
+      alert("Game Over! Your score is: " + score);
+    }
+  }, 1000);
 }
-gethit()
-runtimer()
-makebubble()
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+  if (dets.target.classList.contains("bubble")) {
+    var bubblenumber = Number(dets.target.textContent);
+    if (bubblenumber === ab) {
+      increasescore();
+      gethit(); 
+    }
+  }
+});
+
+gethit();
+runtimer();
+makebubble();
